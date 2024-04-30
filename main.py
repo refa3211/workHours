696bffc9507ea891953accaf20a18099a2fd8277
Refael <refa3211@gmail.com>
2024-04-30 14:02:31 +0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-
+import time
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
@@ -12,6 +13,51 @@ driver_path = r"C:\Users\Refael\AppData\Local\BraveSoftware\Brave-Browser\Applic
 
 
 load_dotenv() 
-username = os.getenv('USER')
-password = os.getenv('PASS')
-OTPCODE = os.getenv('OTPSECERT')
+user_id = os.getenv('ID')
+password = os.getenv('PASSWORD')
+company_id = os.getenv('COMPANY_ID')
+
+options = webdriver.ChromeOptions()
+
+options.add_experimental_option("detach", True)
+
+def login():
+    """login and enter nochchut page
+    
+    """
+    driver = webdriver.Chrome(options=options)  # Assuming you have Chrome WebDriver installed
+    driver.get("https://portal.malam-payroll.com/Salprd2Root/faces/login.jspx")
+    time.sleep(1)
+    
+    
+    company_input = driver.find_element(By.ID, "indexNumInput::content")
+    company_input.send_keys(company_id)
+    time.sleep(1)
+    
+    #username
+    user_id_input = driver.find_element(By.ID, "useridInput::content")
+    user_id_input .send_keys(user_id)
+    time.sleep(1)
+    
+    
+    
+    #password
+    password_input = driver.find_element(By.ID, "it2::content")
+    password_input.send_keys(password) # type: ignore
+    time.sleep(1)
+    #enter
+    password_input.send_keys(Keys.RETURN)
+    time.sleep(1.5)
+    
+    #after login
+    report = driver.find_element(By.ID, "pt1:j_id7")  # Replace "dropdown_id" with the actual ID of the dropdown
+    ActionChains(driver).click(report).perform()
+    time.sleep(1)
+    
+    xpath_nochchut = "/html/body/div[1]/form/div[3]/div[2]/div/div/div/table/tbody/tr/td/table/tbody/tr[2]/td/div/table/tbody/tr[1]/td[2]"
+    nochchut = driver.find_element(By.XPATH, xpath_nochchut)
+    ActionChains(driver).click(nochchut).perform()
+    time.sleep(10)
+
+
+login()
